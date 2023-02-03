9a9af95f0f8933851cb69022d610d8610f879cbd
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-02-03 18:13:39 +0300

    ejs package imported and partials pages modifed

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 
 const app = express()
 
+//template engine
+app.set("view engine","ejs")
+
+app.use(express.static('public'))
+
+
+
+//routes
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'views/index.html'))
+// })
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+    res.render('index')
 })
 
 const port = 3000
